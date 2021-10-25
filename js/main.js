0a7c64083fc86c90bc029e030f63908d2a597a28
LuigiRagusa95 <luigiragusa99@gmail.com>
2021-10-25 14:39:44 +0200

    solved the sum bug 🤦‍♂️

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 let string = '';
-const row = 10;
-const square = 10;
+let sum = 0;
+
+const row = 7;
+const square = 7;
 const board = document.querySelector('.board');
 
 for (let i = 1; i <= row; i++) {
     string += '<div class="row">';
     for (let j = 1; j <= square; j++) {
-        string += `<div class="square bg-square"></div>`;
+        sum += 1;
+        if (sum % 3 === 0 && sum % 5 === 0) {
+            string += `<div class="square bg-fizzbuzz">fizzbuzz</div>`;
+        } else if (sum % 3 === 0) {
+            string += `<div class="square bg-fizz">fizz</div>`;
+        } else if (sum % 5 === 0) {
+            string += `<div class="square bg-buzz">buzz</div>`;
+        } else string += `<div class="square bg-square">${sum}</div>`;
     }
     string += '</div>';
 }
 board.innerHTML += string;
+console.log(sum);
